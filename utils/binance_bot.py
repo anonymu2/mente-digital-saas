72cf9d0ce96e8b00440ef95b0260e4572ffde2df
anonymu2 <76982294+anonymu2@users.noreply.github.com>
2026-04-04 11:07:19 -0400

    binance_bot.py

--- utils/binance_bot.py
+++ utils/binance_bot.py
@@ -1,8 +1,6 @@
 # utils/binance_bot.py
 import random
 
-def get_profit(user):
-    # Aquí va la integración real con Binance
-    profit = round(random.uniform(0, 200), 2)  # Simulación
-    user["profit"] = profit
-    return profit
+# Simula ganancias del bot, luego conecta con Binance real
+def get_profit():
+    return round(random.uniform(0, 200), 2)
