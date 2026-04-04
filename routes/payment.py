c94721d404a58f486109a20a5dd739b6607a6c65
anonymu2 <76982294+anonymu2@users.noreply.github.com>
2026-04-04 08:59:10 -0400

    payment.py

--- routes/payment.py
+++ routes/payment.py
@@ -1,1 +1,14 @@
+from fastapi import APIRouter
+from database import get_db
 
+router = APIRouter()
+
+@router.post("/verify-payment/{email}")
+def verify(email: str):
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("UPDATE users SET is_vip=TRUE WHERE email=%s", (email,))
+    conn.commit()
+
+    return {"status": "VIP ACTIVADO"}
