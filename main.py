1fe97d8308a203e41dff7d9c17e2824404845380
anonymu2 <76982294+anonymu2@users.noreply.github.com>
2026-04-04 11:10:50 -0400

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # main.py
 from fastapi import FastAPI
-from routes import dashboard  # Importamos nuestro dashboard
+from routes import auth, dashboard
 
 app = FastAPI(title="Mente Digital API PRO")
 
+app.include_router(auth.router, prefix="/api")
 app.include_router(dashboard.router, prefix="/api")
 
 @app.get("/")
