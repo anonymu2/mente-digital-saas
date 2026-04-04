d5c9494160e05ec6a7f2068b10ff0eeb1bf44ab2
anonymu2 <76982294+anonymu2@users.noreply.github.com>
2026-04-04 11:45:23 -0400

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# main.py
 from fastapi import FastAPI
-from routes import auth, dashboard
+from routes import auth, dashboard, payment, trading, user
 
 app = FastAPI(title="Mente Digital API PRO")
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(dashboard.router, prefix="/api")
+app.include_router(payment.router, prefix="/api")
+app.include_router(trading.router, prefix="/api")
+app.include_router(user.router, prefix="/api")
 
 @app.get("/")
 def root():
