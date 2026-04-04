855730a9fddca7caab56b118e31afe99240dc116
anonymu2 <76982294+anonymu2@users.noreply.github.com>
2026-04-04 08:52:47 -0400

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-def get_db():
-    return psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
