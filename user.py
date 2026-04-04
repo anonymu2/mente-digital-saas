7c921aba8784b75fde4357b9ebafea0838145166
anonymu2 <76982294+anonymu2@users.noreply.github.com>
2026-04-04 08:39:09 -0400

    user.py

--- user.py
+++ user.py
@@ -1,7 +1,16 @@
 from fastapi import APIRouter
+from database import get_db
 
 router = APIRouter()
 
 @router.get("/user-status/{email}")
 def status(email: str):
-    return {"status": "inactive"}
+    conn = get_db()
+    cur = conn.cursor()
+
+    cur.execute("SELECT is_vip FROM users WHERE email=%s", (email,))
+    user = cur.fetchone()
+
+    return {
+        "status": "active" if user and user["is_vip"] else "inactive"
+    }
