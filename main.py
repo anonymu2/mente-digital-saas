b44734b8221c9f0c750cc98c895a864b474d5932
anonymu2 <76982294+anonymu2@users.noreply.github.com>
2026-04-04 09:06:37 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes import auth, user, trading, payment
+
+app = FastAPI(title="Mente Digital API PRO")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router)
+app.include_router(user.router)
+app.include_router(trading.router)
+app.include_router(payment.router)
+
+@app.get("/")
+def root():
+    return {"status": "API ONLINE 🚀"}
