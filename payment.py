25196393a267a9133f3e64c3a2595f2c5c8fc061
anonymu2 <76982294+anonymu2@users.noreply.github.com>
2026-04-04 08:18:22 -0400

    payment.py

--- payment.py
+++ payment.py
@@ -1,20 +1,7 @@
 from fastapi import APIRouter
-from database import get_db
-from models.schemas import Payment
 
 router = APIRouter()
 
 @router.post("/verify-payment/{email}")
-def verify_payment(email: str, data: Payment):
-    conn = get_db()
-    cur = conn.cursor()
-
-    cur.execute("""
-        UPDATE users 
-        SET is_vip=TRUE, payment_txid=%s
-        WHERE email=%s
-    """, (data.txid, email))
-
-    conn.commit()
-
+def verify(email: str):
     return {"status": "activated"}
